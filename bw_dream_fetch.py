0170f25993a3a8f4a7c7c40ec6f391634af57509
me <me>
2022-04-01 23:04:52 +0200

    feat: add api urls to config file

--- bw_dream_fetch.py
+++ bw_dream_fetch.py
@@ -4,8 +4,16 @@ import requests
 import configparser
 import argparse
 
-regular_game_modes = ["BEDWARS_TWO_FOUR", "BEDWARS_EIGHT_ONE", "BEDWARS_EIGHT_TWO"
+# list all the non-dream gamemodes, to exclude them from count.
+# This allows to prepare for dream modes that may be added in the future
+REGULAR_GAME_MODES = ["BEDWARS_TWO_FOUR", "BEDWARS_EIGHT_ONE", "BEDWARS_EIGHT_TWO"
     , "BEDWARS_FOUR_FOUR", "BEDWARS_EIGHT_TWO_TOURNEY", "BEDWARS_FOUR_THREE"]
+API_ENDPOINT_URLS = {
+    "api_key": "https://api.hypixel.net/key",
+    "games": "https://api.hypixel.net/resources/games",
+    "playercount": "https://api.hypixel.net/counts"
+}
+VERBOSE_MODE = False
 
 
 # basic setup functions
@@ -26,6 +34,7 @@ def create_blank_config_file(filename):
     config["ACCESS"] = {
         "hypixel_api_token": "your_key"
     }
+    config["API_CONFIG"] = API_ENDPOINT_URLS
     with open(filename, "w") as f:
         config.write(f)
 
@@ -47,12 +56,19 @@ def get_api_key_from_file(filename):
     return config["ACCESS"]["hypixel_api_token"]
 
 
+def read_and_replace_url_dict(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    global API_ENDPOINT_URLS
+    API_ENDPOINT_URLS = config["API_CONFIG"]
+
+
 # checking for correctness of elements
-def check_api_working(api_key):
+def check_api_key_working(api_key):
     headers = {
         "API-Key": api_key
     }
-    response = requests.get("https://api.hypixel.net/key", headers=headers)
+    response = requests.get(API_ENDPOINT_URLS["api_key"], headers=headers)
     if response.json()["success"]:
         return True
     else:
@@ -64,7 +80,7 @@ def get_hypixel_playercount(api_key):
     headers = {
         "API-Key": api_key
     }
-    response = requests.get("https://api.hypixel.net/counts", headers=headers)
+    response = requests.get(API_ENDPOINT_URLS["playercount"], headers=headers)
     return response
 
 
@@ -72,7 +88,7 @@ def get_hypxiel_game_modes(api_key):
     headers = {
         "API-Key": api_key
     }
-    response = requests.get("https://api.hypixel.net/resources/games", headers=headers)
+    response = requests.get(API_ENDPOINT_URLS["games"], headers=headers)
     return response.json()["games"]["BEDWARS"]["modeNames"]
 
 
@@ -80,7 +96,7 @@ def get_all_dream_mode_names(api_key):
     results = []
     game_modes = get_hypxiel_game_modes(api_key)
     for i in game_modes.keys():
-        if i not in regular_game_modes:
+        if i not in REGULAR_GAME_MODES:
             results.append(i)
     return results
 
@@ -105,22 +121,28 @@ def extract_highest_dream_game_mode(count_json, api_key):
 
 # main routine functions
 
+def try_read_create_config_file(config_file_name):
+    try:
+        with open(config_file_name) as f:
+            return get_api_key_from_file(config_file_name)
+    except IOError:
+        if not re.match("(.*).ini", config_file_name):
+            config_file_name = config_file_name + ".ini"
+        create_config_file_dialog(config_file_name)
+
+
 def main():
     args = parse_all_args()
     api_key = ""
     if not args.key:
         config_file_name = args.configfile
-        try:
-            with open(config_file_name) as f:
-                api_key = get_api_key_from_file(config_file_name)
-        except IOError:
-            if not re.match("(.*).ini", config_file_name):
-                config_file_name = config_file_name + ".ini"
-            create_config_file_dialog(config_file_name)
+        api_key = try_read_create_config_file(config_file_name)
+        read_and_replace_url_dict(config_file_name)
+        print(API_ENDPOINT_URLS)
     elif args.key:
         api_key = args.key
 
-    if not check_api_working(api_key):
+    if not check_api_key_working(api_key):
         print("Your api key doesn't work, please verify that everything is in order")
         exit(2)
     json = get_hypixel_playercount(api_key).json()
